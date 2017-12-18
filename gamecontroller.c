0795e49ad9fc4ab3251f0ac15ee57d6ca3f6b3ca
BitMaPT <butanotonnsoku@yahoo.co.jp>
2017-12-19 00:04:40 +0900

    upgrade
    upgrade gamecontroller.c main routine added

---
+++ gamecontroller.c
@@ -0,0 +1,73 @@
+#include"ocelo.h"
+
+typedef enum OceloPlayer {
+  PLAYER_BLACK,
+  PLAYER_WHITE
+}OceloPlayer;
+
+void InitGame();
+void WaitPutStone();
+void StartTurn();
+int MousePositionToSquarePosition(int x, int y, int *xx, int *yy);
+void RenewStatus();
+void ExchangeTurn();
+
+OceloPlayer playerTurn = PLAYER_BLACK;
+
+void InitGame() {
+  ocelo[3][3] = STONE_BLACK;
+  ocelo[3][4] = STONE_WHITE;
+  ocelo[4][3] = STONE_WHITE;
+  ocelo[4][4] = STONE_BLACK;
+
+  playerTurn = PLAYER_BLACK;
+}
+
+void StartTurn() {
+  OceloStone stone; 
+
+  if(playerTurn == PLAYER_BLACK) stone = STONE_BLACK;
+  if(playerTurn == PLAYER_WHITE) stone = STONE_WHITE;
+
+  CheckAllSquaresToPut(stone);
+}
+
+void ClickBoard(int x, int y) {
+  int xx, yy;
+  OceloStone stone;
+
+  if(!MousePositionToSquarePosition(x, y, &xx, &yy)) return;
+  if(!oceloCanPut[yy][xx]) return;
+
+  if(playerTurn == PLAYER_BLACK) stone = STONE_BLACK;
+  if(playerTurn == PLAYER_WHITE) stone = STONE_WHITE;
+
+  PutStone(x, y, stone);
+  ExchangeTurn();
+  RenewStatus();
+}
+
+void ExchangeTurn() {
+  switch(playerTurn) {
+    case PLAYER_BLACK: 
+      playerTurn = PLAYER_WHITE;
+      break;
+    case PLAYER_WHITE:
+      playerTurn = PLAYER_BLACK;
+      break;
+  }
+}
+
+void RenewStatus() {
+  OceloStone stone;
+  
+  if(playerTurn == PLAYER_BLACK) stone = STONE_BLACK;
+  if(playerTurn == PLAYER_WHITE) stone = STONE_WHITE;
+
+  CheckAllSquaresToPut(stone);
+}
+
+int MousePositionToSquarePosition(int x, int y, int *xx, int *yy) {
+  
+}
+
