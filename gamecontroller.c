b838a5a279d58e2efc68ac9e011ac6af8157740b
SoyaOnishi <p5h60esu@s.okayama-u.ac.jp>
2017-12-31 13:26:40 +0900

    upgrade
    add state enum

--- gamecontroller.c
+++ gamecontroller.c
@@ -4,15 +4,58 @@
 #include"gamecontroller.h"
 
 //OceloPlayer playerTurn = PLAYER_BLACK;
-static GameState gameState;
 Stone *oceloBoard[OCELO_HEIGHT][OCELO_WIDTH];
 int oceloCanPut[OCELO_HEIGHT][OCELO_WIDTH];
 Object *objectList;
+static GameState gameState;
+
+
+void ControlGameWithState() {
+  switch(gameState.broad) {
+    case GAMESTATE_ALLY:
+      ControlGameWithAllyState();
+      return;
+    case GAMESTATE_REVERSE_ALLY:
+    case GAMESTATE_ENEMY:
+    case GAMESTATE_REVERSE_ENEMY:
+      return;
+  }
+}
+
+void ControlGameWithAllyState() {
+  switch(gameState.detail.allyState) {
+    case ALLYSTATE_SYNC:
+      //sync
+      return;
+    case ALLYSTATE_SHOW_YOURTURN:
+      return;
+    case ALLYSTATE_CHECKING_PUT:
+      
+      return;
+    case ALLYSTATE_WAITING:
+      return;
+    case ALLYSTATE_NONEPUT:
+
+      return;
+  }
+}
+
+void GameControlWithAllyRevState() {
+  switch(gameState.detail.allyRev) {
+    case ALLYREV_READY:
+
+      return;
+    case ALLYREV_ANIMATION:
+
+      return;
+    case ALLYREV_END:
+
+      return;
+  }
+}
 
 void InitGame() {
   int x, y;
-  
-  gameState = GAMESTATE_INIT;
 
   for(y = 0; y < OCELO_HEIGHT; y++) {
     for(x = 0; x < OCELO_WIDTH; x++) {
@@ -20,10 +63,10 @@ void InitGame() {
     }
   }
 
-  oceloBoard[3][3] = InitOceloStone(3, 3, STONE_BLACK);
-  oceloBoard[3][4] = InitOceloStone(3, 4, STONE_WHITE);
-  oceloBoard[4][3] = InitOceloStone(4, 3, STONE_WHITE);
-  oceloBoard[4][4] = InitOceloStone(4, 4, STONE_BLACK);
+  oceloBoard[3][3] = InitOceloStone(3, 3, STONE_COLOR_BLACK);
+  oceloBoard[3][4] = InitOceloStone(3, 4, STONE_COLOR_WHITE);
+  oceloBoard[4][3] = InitOceloStone(4, 3, STONE_COLOR_WHITE);
+  oceloBoard[4][4] = InitOceloStone(4, 4, STONE_COLOR_BLACK);
 
 
 
@@ -31,6 +74,8 @@ void InitGame() {
 
   CheckAllSquaresToPut(STONE_BLACK);
 }
+
+
 
 void ClickBoard(int x, int y) {
   int xx, yy;
@@ -70,4 +115,3 @@ void RenewStatus() {
 int MousePositionToSquarePosition(int x, int y, int *xx, int *yy) {
   
 }
-
--- gamecontroller.h
+++ gamecontroller.h
@@ -3,20 +3,50 @@
 
 #include"ocelostone.h"
 
-typedef enum OceloPlayer {
-  PLAYER_BLACK,
-  PLAYER_WHITE
-}OceloPlayer;
+typedef enum GameBroadState {
+  GAMESTATE_ALLY,
+  GAMESTATE_ENEMY,
+  GAMESTATE_REVERSE_ALLY,
+  GAMESTATE_REVERSE_ENEMY
+}GameBroadState;
 
-typedef enum GameState {
-  GAMESTATE_INIT,
-  GAMESTATE_BLACK_PLAYER,
-  GAMESTATE_WHITE_PLAYER,
-  GAMESTATE_REVERSE_W2B,
-  GAMESTATE_REVERSE_B2W,
-  GAMESTATE_ENDING,
-  GAMESTATE_CALC_RESULT,
-  GAMESTATE_SHOW_RESULT
+typedef enum AllyState {
+  ALLYSTATE_SYNC,
+  ALLYSTATE_SHOW_YOURTURN,
+  ALLYSTATE_CHECKING_PUT,
+  ALLYSTATE_WAITING,
+  ALLYSTATE_NONEPUT,
+}
+
+typedef enum AllyReverse {
+  ALLYREV_READY,
+  ALLYREV_ANIMATION,
+  ALLYREV_END
+}
+
+typedef enum EnemyState {
+  ENESTATE_SYNC,
+  ENESTATE_SHOW_ENETURN,
+  ENESTATE_WATING,
+  ENESTATE_NONEPUT
+}
+
+typedef enum EnemyReverse {
+  ENEREV_READY,
+  ENEREV_ANIMATION,
+  ENEREV_END
+}
+
+typedef union GameDetailState {
+  AllyState allyState;
+  AllyReverse allyRev;
+  EnemyState EneState;
+  EnemyReverse EneRev;
+}GameDetailState;
+
+typedef struct GameState {
+  GameBroadState broad;
+  GameDetailState detail;
 }GameState;
 
 void InitGame();
@@ -26,6 +56,9 @@ void ClickBoard();
 int MousePositionToSquarePosition(int x, int y, int *xx, int *yy);
 void RenewStatus();
 void ExchangeTurn();
+
+void ControlGameWithAllyState();
+void GameControlWithAllyRevState();
 
 extern Stone* oceloBoard[OCELO_HEIGHT][OCELO_WIDTH];
 extern int oceloCanPut[OCELO_HEIGHT][OCELO_WIDTH];
