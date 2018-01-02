1c920af27630edf7f6dc438d082db289a78c4cf7
SoyaOnishi <p5h60esu@s.okayama-u.ac.jp>
2018-01-02 17:16:10 +0900

    upgrade
    put point  routine added

---
+++ putpoint.c
@@ -0,0 +1,64 @@
+#include<GL/glut.h>
+#include<stdlib.h>
+#include<math.h>
+#include"putpoint.h"
+#include"object.h"
+
+int PointStateContena(OceloPoint *point);
+
+void UpdatePoint(OceloPoint *point) {
+  PointStateContena(point);
+}
+
+OceloPoint *InitOceloPoint(int x, int y) {
+  OceloPoint *point;
+  OceloPointState state = {0, 0, 500};
+  Object *obj;
+
+  point = (OceloPoint*)malloc(sizeof(OceloPoint));
+  if(point == NULL) return NULL;
+  obj = (Object*)malloc(sizeof(Object));
+  if(obj == NULL) return NULL;
+
+  point->pos[0] = x;
+  point->pos[1] = y;
+  point->alpha = 1;
+  point->state = state;
+
+  obj->object.point = point;
+  obj->type = OBJECT_SELECTABLE_POINT;
+
+  AddObject(obj);
+
+  return point;
+}
+
+int PointStateContena(OceloPoint *point) {
+  if(point->state.startTime == 0) point->state.startTime = glutGet(GLUT_ELAPSED_TIME);
+
+  point->state.stateTime = glutGet(GLUT_ELAPSED_TIME) - point->state.startTime;
+  point->alpha = 0.5 * sin(2 * M_PI * (double)(point->state.stateTime % point->state.shiftTime) / (double)point->state.shiftTime);
+
+  return 1;
+}
+
+void DrawPoint(OceloPoint *point) {
+  int xx, yy;
+
+  xx = 40 + point->pos[0] * 70 + 35;
+  yy = 40 + point->pos[1] * 70 + 35;
+
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  glTranslated(xx, yy, 0);
+  glColor4d(1, 1, 0, point->alpha);
+  glBegin(GL_QUADS);
+  glVertex2d(xx - 20, yy - 20);
+  glVertex2d(xx - 20, yy + 20);
+  glVertex2d(xx + 20, yy + 20);
+  glVertex2d(xx + 20, yy - 20);
+  glEnd();
+
+  glDisable(GL_BLEND);
+}
--- putpoint.h
+++ putpoint.h
@@ -1,4 +1,22 @@
 #ifndef HEADER_PUTPOINT
 #define HEADER_PUTPOINT
 
+#include<GL/glut.h>
+
+typedef struct OceloPointState {
+  int startTime;
+  int stateTime;
+  int shiftTime;
+}OceloPointState;
+
+typedef struct OceloPoint {
+  GLdouble alpha;
+  OceloPointState state;
+  int pos[2];
+}OceloPoint;
+
+void UpdatePoint(OceloPoint *point);
+OceloPoint *InitOceloPoint(int x, int y);
+void DrawPoint(OceloPoint *point);
+
 #endif
