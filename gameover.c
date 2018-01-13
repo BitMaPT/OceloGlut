b5506b88e0ae71ea623d8c246e83109a36f24d41
Soya-Onishi <p5h60esu@s.okayama-u.ac.jp>
2018-01-13 21:02:07 +0900

    add
    add new file. these files work to manage gameover routine

---
+++ gameover.c
@@ -0,0 +1,109 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"ocelostone.h"
+#include"oceloboard.h"
+#include"object.h"
+#include"mouse.h"
+
+static int oceloResult[2];
+
+int SetResult() {
+  int x, y;
+  int black, white;
+
+  black = 0;
+  white = 0;
+  
+  for(x = 0; x < OCELO_WIDTH; x++) {
+    for(y = 0; y < OCELO_HEIGHT; y++) {
+      if(oceloBoard[y][x] == NULL) continue;
+      switch(oceloBoard[y][x]->stone) {
+        case STONE_COLOR_BLACK:
+          black++;
+          break;
+        case STONE_COLOR_WHITE:
+          white++;
+          break;
+      }
+    }
+  }
+
+  switch(myStoneColor) {
+    case STONE_COLOR_BLACK:
+      oceloResult[0] = black;
+      oceloResult[1] = white;
+      break;
+    case STONE_COLOR_WHITE:
+      oceloResult[0] = white;
+      oceloResult[1] = black;
+      break;
+  }
+
+  DeleteSelectedTypeObject(OBJECT_OCELO_STONE);
+
+  return 1;
+}
+
+int ResultStonePut() {
+  static int startTime = 0;
+  static int mycount = 0, opcount = 0;
+  int shiftTime = 100;
+
+  if(startTime == 0) {
+    startTime = glutGet(GLUT_ELAPSED_TIME);
+  }
+
+  if(oceloResult[0] == 0 && oceloResult[1] == 0) {
+    int nextStateShiftTime = 1000;
+    if(nextStateShiftTime < glutGet(GLUT_ELAPSED_TIME) - startTime) {
+      int pos[] = {WIDTH / 2, HEIGHT / 2};
+      int size[] = {255, 255};
+
+      if(mycount > opcount) {
+        if(InitImage("Stunt.png", pos, size, ImageAnimZoomIn) == NULL) return 0;
+      } else if(mycount == opcount) {
+        if(InitImage("Stunt.png", pos, size, ImageAnimZoomIn) == NULL) return 0;
+      } else {
+        if(InitImage("Stunt.png", pos, size, ImageAnimZoomIn) == NULL) return 0;
+      }
+      gameState = GAMESTATE_RESULT_SHOWRESULT;
+    }
+    return 1;
+  }
+
+  if(shiftTime < glutGet(GLUT_ELAPSED_TIME) - startTime) {
+    startTime = glutGet(GLUT_ELAPSED_TIME);
+
+    if(oceloResult[0] > 0) {
+      int x, y;
+
+      oceloResult[0]--;
+      mycount++;
+
+      x = WIDTH - (mycount % WIDTH);
+      y = HEIGHT - (mycount / WIDTH);
+      InitOceloStone(x, y, myStoneColor);
+    }
+
+    if(oceloResult[1] > 0) {
+      int x, y;
+
+      oceloResult[1]--;
+      opcount++;
+
+      x = opcount % WIDTH;
+      y = opcount / WIDTH;
+      InitOceloStone(x, y, enemyStoneColor);
+    }
+  }
+
+  return 1;
+}
+
+int WaitClickToExit() {
+  int dummy;
+  if(GetMouseDown(GLUT_LEFT_BUTTON, &dummy, &dummy)) {
+    exit(0);
+  }
+  return 1;
+}
---
+++ gameover.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_GAMEOVER
+#define HEADER_GAMEOVER
+
+int SetResult();
+int ResultStonePut();
+int WaitClickToExit();
+
+#endif
