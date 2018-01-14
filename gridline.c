99e0f5dc0048c1398c13209048e47b672ade9bf7
Soya-Onishi <p5h60esu@s.okayama-u.ac.jp>
2018-01-14 14:48:03 +0900

    add
    new file added gridline

---
+++ gridline.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"object.h"
+
+void UpdateGridLine(Object *obj) {
+  //nothing to do
+}
+
+void DrawGridLine(Object *obj) {
+  
+}
+
+void DeleteGridLine(Object *obj) {
+  
+}
---
+++ gridline.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_GLIDLINE
+#define HEADER_GLIDLINE
+
+typedef struct {
+  int pos[2]; //position of upleft
+}GridLine;
+#endif
